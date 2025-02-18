a2b4539fa7997babc8566c327a56f6d607424f67
tm-107 <41205746+tm-107@users.noreply.github.com>
2025-02-18 23:54:46 +0100

    remove entry from rc.local

--- uninstall.sh
+++ uninstall.sh
@@ -6,3 +6,5 @@ rm /service/$SERVICE_NAME
 kill $(pgrep -f 'supervise dbus-evcc')
 chmod a-x $SCRIPT_DIR/service/run
 ./restart.sh
+# remove entry from rc.local
+sed -i '/$SERVICE_NAME/d' /data/rc.local
