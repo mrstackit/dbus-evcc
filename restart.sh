f7adfafdfc6aac2574b4497e6ea8d2d3f0a98ad1
mrstackit <80634441+mrstackit@users.noreply.github.com>
2025-08-03 17:54:08 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-kill $(pgrep -f "python $SCRIPT_DIR/dbus-evcc.py")
+svc -d /service/dbus-evcc
+sleep 1
+svc -u /service/dbus-evcc
